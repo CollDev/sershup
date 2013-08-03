e92043dc13e6251f02c052beeca7f68164499e8d
Joe Robles <joe.robles.pdj@gmail.com>
2013-08-03 17:22:20 -0500

    Goutte implemented

--- src/CollDev/MainBundle/Controller/DefaultController.php
+++ src/CollDev/MainBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace CollDev\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\DomCrawler\Crawler;
+use Goutte\Client;
 
 class DefaultController extends Controller
 {
@@ -14,6 +16,30 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array();
+        $baseURL = 'http://www.serpost.com.pe/';
+        
+        $urlEndpoint = 'calculaRC2/tarifaInternacional2.jsp';
+        $urlMercaderias = 'calculaRC2/tarifaEms2.jsp';
+
+        $interFirstClass = 'body div#result_tarifa table caption';
+        $vigencia = 'div.informacion-servicio div';
+
+        $client = new Client();
+        $crawler = $client->request('GET', $baseURL . $urlMercaderias);
+        $return = $crawler->filter($vigencia)->eq(0)->each(function (Crawler $node, $i) {
+            preg_match('/([0-9]{2})\/([0-9]{2})\/([0-9]{4})/', $node->text(), $matches);
+
+            $baseActualizadaHasta = "08/07/2013";
+            $return['update'] = false;
+            if ($matches[0] > $baseActualizadaHasta) {
+                $return['update'] = true;
+                
+            }
+            $return['vigencia'] = $matches[0];
+            
+            return $return;
+        });
+
+        return array('message' => $return[0]);
     }
 }
